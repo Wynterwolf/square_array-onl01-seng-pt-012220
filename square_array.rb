1de4ef95cfe0483054e73b3c882708d3fd6c82c1
Samantha Jordan <shinyguns@gmail.com>
2020-01-26 06:59:54 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,12 @@
 def square_array(array)
   # your code here
+<<<<<<< HEAD
   new_array = []
   array.each do |int|
     new_array << int ** 2
   end
   return new_array
+=======
+  
+>>>>>>> 1362a46ea53dd45b35299c93b1646bbb4adabccb
 end
